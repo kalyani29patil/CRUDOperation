132d4ac2964004e56dc6819c8b24b22ac88266a3
kalyanipatil <kalyani29patil@gmail.com>
2024-06-23 22:32:34 +0530

    commit for Controller layer done

--- src/main/java/com/bikkadit/demo1/repository/UserRepository.java
+++ src/main/java/com/bikkadit/demo1/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.bikkadit.demo1.repository;
 
 import com.bikkadit.demo1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
