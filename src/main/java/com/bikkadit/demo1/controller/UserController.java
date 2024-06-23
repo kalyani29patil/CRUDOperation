484737e5524a7c10dd98245a9aba32998abb9b3d
kalyanipatil <kalyani29patil@gmail.com>
2024-06-23 22:22:19 +0530

    commit for Controller layer done

--- src/main/java/com/bikkadit/demo1/controller/UserController.java
+++ src/main/java/com/bikkadit/demo1/controller/UserController.java
@@ -4,10 +4,11 @@ package com.bikkadit.demo1.controller;
 import com.bikkadit.demo1.model.User;
 import com.bikkadit.demo1.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,10 +17,43 @@ public class UserController {
     @Autowired
     private UserServiceI userServiceI;
 
-    @PostMapping("/")
-    public ResponseEntity<User>  createUser(){
+    @PostMapping("/users")
+    public ResponseEntity<User>  createUser(@RequestBody User user){
+        User saveduser = userServiceI.createUser(user);
+        return new ResponseEntity<>(saveduser, HttpStatus.CREATED);
 
-        return null;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> allUsers = userServiceI.getAllUsers();
+
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @GetMapping("users/{userId}")
+    public ResponseEntity<User> getSingleUser(@PathVariable Long userId){
+        User user = userServiceI.getSingleUser(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/users/{userId}")
+    public ResponseEntity<User> updateUser(@RequestBody User user,@PathVariable Long userId){
+        User updatedUser = userServiceI.updateUser(user, userId);
+        return new ResponseEntity<>(updatedUser, HttpStatus.CREATED);
+
+
+    }
+    //@pathvariable= pass single data     /,/
+    //@requestBody=whole object
+    //@Requestparam=key-value pair=for paggination and sorting    ?,&
+
+    @DeleteMapping("users/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId){
+        userServiceI.deleteUser(userId);
+        return new ResponseEntity<>( "Resource deleted succesfully", HttpStatus.OK);
+
 
     }
 }
--- src/main/java/com/bikkadit/demo1/service/impl/UserServiceImpl.java
+++ src/main/java/com/bikkadit/demo1/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import com.bikkadit.demo1.model.User;
 import com.bikkadit.demo1.repository.UserRepository;
 import com.bikkadit.demo1.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserServiceI {
 
    @Autowired
